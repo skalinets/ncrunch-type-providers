d2dccfad966fe0197de14a2f5adef402954024f4
Serhiy Kalinets <kalinets@gmail.com>
2014-12-23 16:20:48 +0200

    initial commit

---
+++ ncrunch-type-providers/Tests/Tests.fs
@@ -0,0 +1,12 @@
+﻿module Tests
+open Xunit
+open FSharp.Data
+
+type Simple = JsonProvider<""" { "name":"John", "age":94 } """>
+
+[<Fact>]
+let test () =
+  let simple = Simple.Parse(""" { "name":"Tomas", "age":4 } """)
+  Assert.Equal(4,  simple.Age)
+  Assert.Equal<string>("Tomas",  simple.Name)
+  
